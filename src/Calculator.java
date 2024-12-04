d19302a8ff8f1690a7e27d935a944f9fa835f355
Hector <54409777b@iespoblenou.org>
2024-12-04 10:21:54 +0100

    "Added modulus operation"

--- src/Calculator.java
+++ src/Calculator.java
@@ -15,13 +15,18 @@ public class Calculator {
         }
         return a / b;
     }
+    public int modulus(int a, int b) {
+        return a % b;
+    }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println("Addition: " + calculator.add(2, 3));
-        System.out.println("Subtraction: " + calculator.subtract(5, 3));
-        System.out.println("Multiplication: " + calculator.multiply(2, 3));
+            System.out.println("Addition: " + calculator.add(2, 3));
+            System.out.println("Subtraction: " + calculator.subtract(5, 3));
+            System.out.println("Multiplication: " + calculator.multiply(2, 3));
             System.out.println("Division: " + calculator.divide(6, 3));
             System.out.println("Division zero: " + calculator.divide(6, 0));
+             System.out.println("Modulus: " + calculator.modulus(10, 3));
+
         }
 
     }
